bba6c60ea5e7655c6d881a280d40ae9c272880eb
Taiman Saidaliyev <taimansaidaliev@gmail.com>
2024-08-27 10:53:25 +0500

    update banner model change title

--- banners/models.py
+++ banners/models.py
@@ -8,7 +8,7 @@ class Banners(models.Model):
     description = models.CharField(max_length=1000, verbose_name='Описание')
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Магазин', related_name='shop_banner')
     link = models.CharField(max_length=300, verbose_name='Ссылка')
-    photo = models.ImageField(upload_to='banners/%Y/%m/%d', verbose_name='Изображение', blank=True)
+    photo = models.ImageField(upload_to='prbn/%Y/%m/%d', verbose_name='Изображение', blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
     city = models.ForeignKey(City, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Город', related_name='banner_city')
